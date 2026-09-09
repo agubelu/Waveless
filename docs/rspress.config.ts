f919542a4916c6b9f4e28a6313c3ad104f08c73e
Oscar <36568572+nv0skar@users.noreply.github.com>
2026-09-10 00:11:05 +0200

    - Updated CI workflow. - Updated `.gitignore`.

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -7,9 +7,15 @@ import { pluginLlms } from "@rspress/plugin-llms";
 
 export default defineConfig({
     root: path.join(__dirname, "src"),
+    outDir: path.join(__dirname, "doc_build"),
     title: "Waveless",
     plugins: [katex(), viz(), pluginLlms()],
     globalStyles: path.join(__dirname, "theme/main.css"),
+    builderConfig: {
+        output: {
+            assetPrefix: "auto",
+        },
+    },
     themeConfig: {
         lastUpdated: {
             author: true,
